f9e8837ceba945b942f40e7ada9da47c7e8c1957
Shreyas <shreyas.aws.devops@gmail.com>
2025-09-02 18:10:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "docker_vm" {
   ami                     = "ami-0861f4e788f5069dd"
   instance_type           = "t2.micro"
   vpc_security_group_ids  = ["sg-0137a88aa4803802d"]
@@ -15,6 +15,18 @@ resource "aws_instance" "app_server" {
 
 }
 
+resource "aws_instance" "minikube_vm" {
+  ami                     = "ami-0861f4e788f5069dd"
+  instance_type           = "t2.medium"
+  vpc_security_group_ids  = ["sg-0137a88aa4803802d"]
+  user_data               = file("${path.module}/install_minikube.sh") 
+ 
+
+  tags = {
+    Name = "MyEc2Instance"
+  }
+
+}
 
 
 
