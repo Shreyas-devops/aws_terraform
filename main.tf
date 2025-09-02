6aaea79dfbd3b43cda6c560cb1fb133c2899cf2a
Shreyas <shreyas.aws.devops@gmail.com>
2025-09-02 17:42:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,35 +6,37 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0861f4e788f5069dd"
   instance_type = "t2.micro"
   vpc_security_group_ids = [sg-0137a88aa4803802d]
-  user_data = << EOF #!/bin/bash
+  user_data = << EOF 
+      #!/bin/bash
      sudo yum update -y
      echo 'Installing docker on $(hostname)'
-    sudo yum install docker -y
-    if [ $? -eq 0 ]; then
-    echo 'Docker is installed ..'
-    sudo usermod -aG docker ec2-user
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    sudo service docker status
-    echo 'docker version is '
-    sudo docker -v
-    echo 'Running Docker hello-world'
-    docker  run 'hello-world'
-    echo 'Installing Docker-compose...'
-    sudo curl -L "https://github.com/docker/compose/releases/download/v2.39.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    echo 'Docker Compose Installed.'
-    docker-compose --version
-    echo '=================================================================================='
-    else
-    echo 'Docker installation failed on $(hostname)'
-    echo '=================================================================================='
-    fi> /tmp/user_data_output.txt
-      EOF
+     sudo yum install docker -y
+     if [ $? -eq 0 ]; then
+     echo 'Docker is installed ..'
+     sudo usermod -aG docker ec2-user
+     sudo systemctl start docker
+     sudo systemctl enable docker
+     sudo service docker status
+     echo 'docker version is '
+     sudo docker -v
+     echo 'Running Docker hello-world'
+     docker  run 'hello-world'
+     echo 'Installing Docker-compose...'
+     sudo curl -L "https://github.com/docker/compose/releases/download/v2.39.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+     sudo chmod +x /usr/local/bin/docker-compose
+     echo 'Docker Compose Installed.'
+     docker-compose --version
+     echo '=================================================================================='
+     else
+     echo 'Docker installation failed on $(hostname)'
+     echo '=================================================================================='
+     fi> /tmp/user_data_output.txt
+     EOF
 
   tags = {
     Name = "MyEc2Instance"
   }
 
 }
+
 
