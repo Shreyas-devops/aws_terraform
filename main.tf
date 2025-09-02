f1d48627d4487769d54fa3891dd7681d4b41ed72
Shreyas <shreyas.aws.devops@gmail.com>
2025-09-02 17:53:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami                     = "ami-0861f4e788f5069dd"
   instance_type           = "t2.micro"
-  vpc_security_group_ids  = [sg-0137a88aa4803802d]
+  vpc_security_group_ids  = ["sg-0137a88aa4803802d"]
   user_data               = file("${path.module}/install_docker.sh") 
  
 
@@ -14,6 +14,7 @@ resource "aws_instance" "app_server" {
   }
 
 }
+
 
 
 
