3cd73b8da41a650ea089b6452f634a76d0e4b65a
Shreyas <shreyas.aws.devops@gmail.com>
2025-09-23 10:44:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "myvmpublickey"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKPWQrWfhG7mERSvofpgZ/06kyIXeF7uWO/VHnQJnzD3AkTS+yWpIDxpRnLJEVNc0JiwpoSKF/ci+1yLPVbqrX9/lvBqZfybvPFMqNZTaabhAGMxX3G/SiXAwq517U1F2Von8hYhXIoIlEnhwSmFYK/wZFzsoKR8uoo1vLSaY9QqiA9OwlAKV1l2xX9UaGCHnhsr4ejAWb2pjOHX8ClICbgwEr2ATNkRo98vkKGPt5TjwKGhUgKVsVx7kx1k+TKam5GgpjQouj3YJteB0rI/kawMas1DOMNCuYq4dnggYYflOajaml1mMYPP3BXMJeOFZMlKbbIwT9nWnab6867bYhg2UY2oLzYV8MzT1IuXmQG05yIwDfb6lEE/SsQuQUMAOj1LMn85bps+nM+eamrdUGFcbeyF5nk+liweJcetliVsHvLehBQu1w9C60kJMtDlfEVYBwq1nsCeh/5YPe24TpOwdAsZa9CFvw2B10/I03orxGchnIKPb3fS2wzBBPrH0= osboxes@ansiblecontroller"
+  public_key = file("${path.module}/sshkey.pub") 
 }
 
 resource "aws_instance" "docker_vm" {
@@ -39,6 +39,7 @@ output "instance_public_ip" {
 
 }
 */
+
 
 
 
