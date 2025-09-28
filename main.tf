4d423d50131779b716b995b3353f546822335217
Shreyas <shreyas.aws.devops@gmail.com>
2025-09-28 17:07:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,9 +26,10 @@ output "instance_public_ip" {
   value       = aws_instance.docker_vm.public_ip
 }
 
-/*resource "aws_instance" "minikube_vm" {
+resource "aws_instance" "minikube_vm" {
   ami                     = "ami-0861f4e788f5069dd"
   instance_type           = "t2.medium"
+  key_name                = aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids  = ["sg-0137a88aa4803802d"]
   user_data               = file("${path.module}/install_minikube.sh") 
  
@@ -38,7 +39,18 @@ output "instance_public_ip" {
   }
 
 }
-*/
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.docker_vm.public_ip
+}
+
+output "instance_public_ip2" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.minikube_vm.public_ip
+}
+
+
 
 
 
